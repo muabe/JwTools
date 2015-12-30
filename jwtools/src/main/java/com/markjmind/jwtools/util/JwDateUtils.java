3864a2f6d1b614a54191efe1e43bea3c9e742be5
MarkJ <markjmind@gmail.com>
2015-12-30 10:01:53 +0900

    날짜사이계산 추가

--- jwtools/src/main/java/com/markjmind/jwtools/util/JwDateUtils.java
+++ jwtools/src/main/java/com/markjmind/jwtools/util/JwDateUtils.java
@@ -1,5 +1,6 @@
 package com.markjmind.jwtools.util;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
@@ -21,5 +22,82 @@ public class JwDateUtils {
         return JwDateUtils.getFormat(date, format, Locale.KOREA);
     }
 
+    public static int betweenSec(Date from, Date to){
+        long duration = from.getTime() - to.getTime();
+        return (int) (duration / (1000));
+    }
 
+    public static int betweenMin(Date from, Date to){
+        long duration = from.getTime() - to.getTime();
+        return (int) (duration / (1000 * 60));
+    }
+
+    public static int betweenHour(Date from, Date to){
+        long duration = from.getTime() - to.getTime();
+        return (int) (duration / (1000 * 60 * 60));
+    }
+
+    public static int betweenDay(Date from, Date to){
+        long duration = from.getTime() - to.getTime();
+        return (int) (duration / (1000 * 60 * 60 *24));
+    }
+
+    public static int betweenMonth(Date from, Date to){
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", java.util.Locale.KOREA);
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MM", java.util.Locale.KOREA);
+        SimpleDateFormat dayFormat = new SimpleDateFormat("dd", java.util.Locale.KOREA);
+
+        int fromYear = Integer.parseInt(yearFormat.format(from));
+        int toYear = Integer.parseInt(yearFormat.format(to));
+        int fromMonth = Integer.parseInt(monthFormat.format(from));
+        int toMonth = Integer.parseInt(monthFormat.format(to));
+        int fromDay = Integer.parseInt(dayFormat.format(from));
+        int toDay = Integer.parseInt(dayFormat.format(to));
+
+        int result = 0;
+        result += ((toYear - fromYear) * 12);
+        result += (toMonth - fromMonth);
+
+        //        if (((toDay - fromDay) < 0) ) result += fromDate.compareTo(toDate);
+        // ceil과 floor의 효과
+        if (((toDay - fromDay) > 0))
+            result += to.compareTo(from);
+
+        return result;
+    }
+
+    public static int betweenYear(Date from, Date to){
+        long duration = from.getTime() - to.getTime();
+        return (int) (duration / (1000 * 60 * 60 * 24 * 356));
+    }
+
+    public static boolean equalsSec(Date a, Date b){
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyyMMddhhMMss", java.util.Locale.KOREA);
+        return yearFormat.format(a).equals(yearFormat.format(b));
+    }
+
+    public static boolean equalsMin(Date a, Date b){
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyyMMddhhMM", java.util.Locale.KOREA);
+        return yearFormat.format(a).equals(yearFormat.format(b));
+    }
+
+    public static boolean equalsHour(Date a, Date b){
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyyMMddhh", java.util.Locale.KOREA);
+        return yearFormat.format(a).equals(yearFormat.format(b));
+    }
+
+    public static boolean equalsDay(Date a, Date b){
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyyMMdd", java.util.Locale.KOREA);
+        return yearFormat.format(a).equals(yearFormat.format(b));
+    }
+
+    public static boolean equalsMonth(Date a, Date b){
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyyMM", java.util.Locale.KOREA);
+        return yearFormat.format(a).equals(yearFormat.format(b));
+    }
+
+    public static boolean equalsYear(Date a, Date b){
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", java.util.Locale.KOREA);
+        return yearFormat.format(a).equals(yearFormat.format(b));
+    }
 }
